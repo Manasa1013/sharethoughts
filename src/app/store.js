4d21a6d78121e51bc6ff417bc18924a970333fa2
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2021-08-07 19:22:02 +0530

    store and styles updation

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import postsReducer from "../features/posts/postsSlice";
+import usersReducer from "../features/users/usersSlice";
+import notificationsReducer from "../features/notifications/notificationsSlice";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+export const store = configureStore(
+  {
+    reducer: {
+      posts: postsReducer,
+      users: usersReducer,
+      notifications: notificationsReducer,
+    },
   },
-});
+  applyMiddleware(thunk)
+);
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { fetchUsers } from "./features/users/usersSlice";
+import { fetchPosts } from "./features/posts/postsSlice";
 
+console.log(
+  store
+    .dispatch(fetchUsers())
+    .then((data) => console.log(data.payload, "from then of store")),
+  "from index.js"
+);
+store.dispatch(fetchPosts());
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
