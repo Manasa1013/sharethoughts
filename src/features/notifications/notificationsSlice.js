6003c8eab9acba76380f7f470b4ae2e6e4f048a5
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2021-08-07 19:24:20 +0530

    notifications slice updation

---
+++ src/features/notifications/notificationsSlice.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  notifications: [],
+  status: "idle",
+  error: null,
+};
+const apiServerUrl = "https://apiforstocks-1.manasa1998.repl.co";
+
+export const fetchNotifications = createAsyncThunk(
+  "notifications/fetchNotifications",
+  async () => {
+    try {
+      const response = await axios.get(apiServerUrl + "/notifications");
+      console.log(response.data, "form notifications");
+      if (response.status === 200) {
+        console.log("notifs can be retrieved", response.data.notificationsData);
+        return response.data.notificationsData;
+      }
+    } catch (err) {
+      console.error("fetchNotifs catch error ", err);
+      return { err };
+    } finally {
+      console.log("executed finally from fetchNotifs");
+    }
+  }
+);
+export const notificationsSlice = createSlice({
+  name: "notifications",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchNotifications.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state.notifications = state.notifications.concat(action.payload);
+    },
+    [fetchNotifications.pending]: (state) => {
+      state.status = "loading";
+    },
+    [fetchNotifications.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.payload;
+    },
+  },
+});
+
+export default notificationsSlice.reducer;
