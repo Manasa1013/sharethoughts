f2a7063619599a74434bbe86e5c960ec2eb49870
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2021-08-07 19:22:38 +0530

    routes configured

---
+++ src/app/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import { fetchNotifications } from "../features/notifications/notificationsSlice";
+export const Navbar = () => {
+  const dispatch = useDispatch();
+  let { pathname } = useLocation();
+  console.log(pathname, "from navbar path");
+  const [refreshNotifs, setRefreshNotifs] = useState(true);
+
+  return (
+    <nav>
+      <section className="nav--container">
+        <h2> Practice Redux</h2>
+        <div className="nav--contents">
+          <div className="nav--links">
+            <Link className="button button__secondary" to="/">
+              Posts
+            </Link>
+            <Link className="button button__secondary" to="/users">
+              Users
+            </Link>
+            <Link className="button button__secondary" to="/notifications">
+              Notifications
+            </Link>
+            {pathname === "/notifications" && (
+              <button
+                type="button"
+                className="button button__Link"
+                onClick={(prev) => {
+                  setRefreshNotifs(!prev);
+                  dispatch(fetchNotifications());
+                }}
+              >
+                Refresh Notifications
+              </button>
+            )}
+          </div>
+        </div>
+      </section>
+    </nav>
+  );
+};
